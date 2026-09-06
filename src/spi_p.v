570cd395bbc1f8d962852f1612b67cc7a70596f4
Andrew Zhu <andrew.zhu739@gmail.com>
2026-09-06 01:17:50 +0000

    shrink count by 1 wire

--- src/spi_p.v
+++ src/spi_p.v
@@ -19,7 +19,7 @@ localparam   state_error=2'b11,
 reg [1:0] current_state;
 reg [14:0] data_stored;
 
-reg [3:0] bit_count;
+reg [2:0] bit_count;
 
 reg [1:0] data_sync;
 reg [1:0] cs_sync;
@@ -50,7 +50,7 @@ always @(posedge clk or negedge rst_n) begin
 
     if(!rst_n) begin
         current_state <= state_sample_addr;
-        bit_count<=4'd0;
+        bit_count<=3'd0;
         data_stored<=15'b0;
         en_reg_out_7_0<=8'b0;
         en_reg_out_15_8<=8'b0;
@@ -71,7 +71,7 @@ always @(posedge clk or negedge rst_n) begin
                 endcase
         end
         current_state <= state_sample_addr;
-        bit_count<=4'd0;
+        bit_count<=3'd0;
         data_stored<=15'b0;
         transaction_ready <= 1'b0;
     end else begin
@@ -79,30 +79,30 @@ always @(posedge clk or negedge rst_n) begin
             state_sample_addr: begin
                 if(sclk_rising_edge) begin 
                     data_stored <= {data_stored[13:0],sync_data};
-                    if(bit_count== 4'b0 && sync_data==1'b0) begin
+                    if(bit_count== 3'b0 && sync_data==1'b0) begin
                         current_state <= state_error;
-                    end else if(bit_count==4'd7) begin
+                    end else if(bit_count==3'd7) begin
                         
                         if({data_stored[5:0],sync_data}>MAX_ADDRESS) begin
                             current_state <= state_error;
                         end else begin
                             current_state <= state_sample_data;
-                            bit_count <= bit_count + 4'd1;
+                            bit_count <= 3'b0;
                         end
                     end else begin
                         
-                        bit_count <= bit_count + 4'd1;
+                        bit_count <= bit_count + 3'b1;
                     end
                 end
             end
             state_sample_data: begin
                 if(sclk_rising_edge) begin
                     data_stored <= {data_stored[13:0],sync_data};
-                    if(bit_count==4'd15) begin
+                    if(bit_count==3'd7) begin
                         current_state<=state_sample_addr;
                         transaction_ready<=1'b1;
                     end else begin
-                        bit_count <= bit_count + 4'd1;
+                        bit_count <= bit_count + 3'd1;
                     end
                 end
             end
